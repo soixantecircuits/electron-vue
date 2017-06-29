7023bab2a341dbcb1d8a6d111a3c780ffb3e1bd0
yael <yael@soixantecircuits.fr>
2017-06-29 12:05:39 +0200

    :shirt: ignore eslint errors in settings.js

--- template/src/renderer/lib/settings.js
+++ template/src/renderer/lib/settings.js
@@ -8,12 +8,16 @@ if (process.env.IS_WEB || process.env.NODE_ENV === 'testing') {
   settings = SETTINGS
   /* eslint-enable */
 } else {
+  /* eslint-disable */
   var remote = require('electron').remote
+  /* eslint-enable */
   settings = remote.getGlobal('settings')
 }
 
 if (process.env.debug) {
+  /* eslint-disable */
   console.log('loaded settings: ', settings)
+  /* eslint-enable */
 }
 
 module.exports = settings
