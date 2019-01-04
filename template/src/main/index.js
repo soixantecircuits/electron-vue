b3eabd07789577ced100f7fe8640d6a30c915142
gabrielstuff <gabriel@soixantecircuits.fr>
2019-01-04 15:45:19 +0100

    💎 add exit code and app.quit for thrown errors

--- template/src/main/index.js
+++ template/src/main/index.js
@@ -20,38 +20,57 @@ function createWindow () {
   /**
    * Initial window options
    */
-  let options = {
-    height: 563,
-    useContentSize: true,
-    width: 1000
-  }
-  {{#isEnabled plugins 'standard-settings'}}
-  options = require('assignment')(options, global.settings.window)
-  mainWindow = new BrowserWindow(options) 
-  if (global.settings.simulateOutage) {
-    mainWindow.webContents.session.enableNetworkEmulation({offline: true})
-  }
-  if (global.settings.openDevTools) {
-    mainWindow.webContents.openDevTools()
-  }
-  if (global.settings.clearCache) {
-    console.warn('⚠️ - Option to clear cache set, cache will be cleared')
-    mainWindow.webContents.session.clearStorageData({}, () => {
-      mainWindow.webContents.session.clearCache(() => {
-        console.log('cache cleared.')
-        mainWindow.loadURL(winURL)
+  if(!mainWindow) {
+    console.info('🌱 - instantiate new window from createWindow function')
+    let options = {
+      height: 563,
+      useContentSize: true,
+      width: 1000
+    }
+    {{#isEnabled plugins 'standard-settings'}}
+    options = require('assignment')(options, global.settings.window)
+    mainWindow = new BrowserWindow(options) 
+    if (global.settings.simulateOutage) {
+      mainWindow.webContents.session.enableNetworkEmulation({offline: true})
+    }
+    if (global.settings.openDevTools) {
+      mainWindow.webContents.openDevTools()
+    }
+    if (global.settings.clearCache) {
+      console.warn('⚠️ - Option to clear cache set, cache will be cleared')
+      mainWindow.webContents.session.clearStorageData({}, () => {
+        mainWindow.webContents.session.clearCache(() => {
+          console.log('cache cleared.')
+          mainWindow.loadURL(winURL)
+        })
       })
-    })
-  } else {
+    } else {
+      mainWindow.loadURL(winURL)
+    }
+    {{else}}
     mainWindow.loadURL(winURL)
+    {{/isEnabled}}
+    
+    mainWindow.on('closed', () => {
+      mainWindow = null
+    })
+    mainWindow.on('unresponsive', () => {
+      console.error('ERROR 61 - Window does not respond, let\'s quit')
+      app.quit()
+    })
+
+    mainWindow.webContents.on('crashed', () => {
+      console.error('ERROR 62 - Webcontent renderer crashed, let\'s quit')
+      app.quit()
+    })
+
+    mainWindow.webContents.on('destroyed', () => {
+      console.error('ERROR 63 - Webcontent destroyed, let\'s quit')
+      app.quit()
+    })
+  }else {
+    console.warn('⚠️ - trying to create a new Window from create window. Window already exist. Not creating')
   }
-  {{else}}
-  mainWindow.loadURL(winURL)
-  {{/isEnabled}}
-  
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
 }
 {{#isEnabled plugins 'standard-settings'}}
 global.settings = require('standard-settings').getSettings()
@@ -72,6 +91,11 @@ if (global.settings.appendArgument) {
 {{/isEnabled}}
 
 app.on('ready', createWindow)
+
+app.on('gpu-process-crashed', () => {
+  console.error('ERROR 64 - App GPU process has crashed, let\'s quit')
+  app.quit()
+})
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
