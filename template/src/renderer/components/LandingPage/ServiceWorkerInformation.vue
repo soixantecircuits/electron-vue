1c973900fa8c98438ad152d266f805548895a9aa
gabrielstuff <gabriel@soixantecircuits.fr>
2019-05-22 10:16:18 +0200

    💅 make ESLint airbnb compliant

--- template/src/renderer/components/LandingPage/ServiceWorkerInformation.vue
+++ template/src/renderer/components/LandingPage/ServiceWorkerInformation.vue
@@ -6,12 +6,16 @@
     <div v-if="!settings.serviceWorker.cacheFirst">
       Service worker is enabled wih priority to network, and fallback on cache if no network.<br/>
       1. Refresh this page and you will see a new picture, refresh again, and again. <br/>
-      2. Disconnect from network, refresh and you will still see the last picture, it's not gone. Refresh and refresh, it is still here.<br/>
-      3. You can also run service worker with priority to cache <div class="value">yarn dev --serviceWorker.cacheFirst true</div>.
+      2. Disconnect from network, refresh and you will still see the last picture, it's not gone.
+      Refresh and refresh, it is still here.<br/>
+      3. You can also run service worker with priority to cache
+      <div class="value">yarn dev --serviceWorker.cacheFirst true</div>.
     </div>
     <div v-else>
       Service worker is enabled with priority to cache, so content is only fetched once.<br/>
-      1. Refresh this page. If this is the first time you run the app, the image above was downloaded before the service-worker was installed, so it will change once: refresh again.<br/>
+      1. Refresh this page. If this is the first time you run the app, the image above
+      was downloaded before the service-worker was installed, so it will change once:
+      refresh again.<br/>
       2. The image does not change. Refresh again and again, it is still the same.
     </div>
   </div>
